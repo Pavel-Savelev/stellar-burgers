172d9daec4766277c50c1a9cf14c6ba66c4bdab8
Pavel Savelev <9173912682pavel@gmail.com>
2025-08-18 14:37:02 +0300

    route updated

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,36 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, Login, Register, ForgotPassword, ResetPassword, NotFound404,Profile,ProfileOrders, } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader,OrderInfo,IngredientDetails,Modal } from '@components';
+
+import { Routes, Route } from 'react-router-dom';
+
+import { ProtectedRoute } from '../ui/protect-router';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<ProtectedRoute><Login /></ProtectedRoute>} />
+        <Route path='/register' element={<ProtectedRoute><Register /></ProtectedRoute>} />
+        <Route path='/forgot-password' element={<ProtectedRoute><ForgotPassword /></ProtectedRoute>} />
+        <Route path='/reset-password' element={<ProtectedRoute><ResetPassword /></ProtectedRoute>} />
+        <Route path='/profile/orders' element={<ProtectedRoute><ProfileOrders /></ProtectedRoute>} />
+        <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path='*' element={<NotFound404 />} />
+
+        {/* <Route path='/feed/:number' element={<Modal>{<OrderInfo/>}</Modal>} />
+        <Route path='/ingredients/:id' element={<Modal><IngredientDetails/></Modal>} />
+        <Route path='/profile/orders/:number' element={<Modal><OrderInfo/></Modal>} /> */}
+    </Routes>
   </div>
 );
+
+// TODO Potected router
+// links +
+//  Означает layout что все внуттри будет иметь свою шапку и тд
 
 export default App;
